31f7577a5f81079063a5834134205c06a2f7ff5f
03one02one <ehrkd1021@naver.com>
2021-05-04 12:07:05 +0900

    #19 삼항연산자 실습

--- CSBasic2/Program.cs
+++ CSBasic2/Program.cs
@@ -77,6 +77,11 @@ namespace CSBasic2
                     break;
             }
 
+            string input2 = Console.ReadLine();
+            int number = int.Parse(input2);
+            Console.WriteLine((number > 0) ? "자연수" : "자연수 아님");
+            // Console.WriteLine((number > 0) ? a() : bool());
+
         }
     }
 }
