b4fb8de5ca382f6eecdd11135f131e9dd11fcda3
03one02one <ehrkd1021@naver.com>
2021-06-22 12:21:09 +0900

    # 33 메서드 기본

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -8,8 +8,33 @@ namespace CSBasic5
 {
     class Program
     {
+        class MethodExample
+        {
+            public int Power(int x)
+            {
+                return x * x;
+            }
+
+            public int Multi(int x, int y)
+            {
+                return x * y;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine("Print() 메서드 호출됨");
+            }
+        }
+
+
         static void Main(string[] args)
         {
+            MethodExample me = new MethodExample();
+            Console.WriteLine(me.Power(10));
+            Console.WriteLine(me.Power(20));
+            Console.WriteLine(me.Multi(52, 273));
+            Console.WriteLine(me.Multi(103, 32));
+            me.Print();
         }
     }
 }
