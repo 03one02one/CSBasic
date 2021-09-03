4e1d36a0aa0d11038c6944d860ef029f27b8fd2b
03one02one <ehrkd1021@naver.com>
2021-09-03 15:40:58 +0900

    윈폼 Label, LinkLabel 및 명령어 실행 예제

--- CSBasic8-label/Form1.cs
+++ CSBasic8-label/Form1.cs
@@ -15,6 +15,28 @@ namespace CSBasic8_label
         public Form1()
         {
             InitializeComponent();
+
+            Label label = new Label()
+            {
+                Text = "글자", Location = new Point(10, 10)
+            };
+            //label.Text = "글자";
+            //label.Location = new Point(10, 10);
+            Controls.Add(label);
+
+            LinkLabel linkLabel = new LinkLabel()
+            {
+                Text = "글자",
+                Location = new Point(10, 50)
+            };
+            Controls.Add(linkLabel);
+
+            linkLabel.Click += LabelClick;
+        }
+        private void LabelClick(object sender, EventArgs s)
+        {
+            System.Diagnostics.Process.Start("https://www.naver.com");
+            System.Diagnostics.Process.Start("notepad.exe");
         }
     }
 }
